081bb200327236a5c184ef8e4607492fe7d65b29
Lee Turner <lvturner@hey.com>
2024-10-12 15:31:05 +0800

    Run commands using VSCode terminal Now supports 'forge tunne' and 'forge deploy'

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Basic tools for working with Atlassian Forge
 
-The end!
+Use Ctrl+Shift+P to open the command palette.
+
+There are currently two commands available
+1. Run Forge Tunnel
+2. Run Forge Deploy
+
+Neither take options and will open a new terminal running in their default configuration
--- src/extension.ts
+++ src/extension.ts
@@ -2,32 +2,31 @@ import * as vscode from 'vscode';
 import { exec } from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('atlassian-forge.runForgeTunnel', (args) => {
-    runForgeTunnel().then((output) => {
-      console.log(output);
-      if (vscode.window.activeTextEditor) {
-        const selection = vscode.window.activeTextEditor.selection;
-        vscode.window.showInformationMessage(`Selected text: ${selection}`);
-      }
-    }).catch((error) => {
-      console.error(error);
-      vscode.window.showErrorMessage('Failed to run shell command.');
-    });
-  });
+  const commands = [
+    vscode.commands.registerCommand('atlassian-forge.runForgeTunnel', () => {
+      runForgeTunnel();
+    }),
 
-  context.subscriptions.push(disposable);
+    vscode.commands.registerCommand('atlassian-forge.runForgeDeploy', () => {
+      runForgeDeploy();
+    })
+  ];
+
+  for (const command of commands) {
+    context.subscriptions.push(command);
+  }
 }
 
-function runForgeTunnel(): Promise<string> {
-  return new Promise((resolve, reject) => {
-    exec('forge tunnel', (error, stdout, stderr) => {
-      if (error) {
-        reject(stderr || error.message);
-        return;
-      }
-      resolve(stdout ? stdout : stderr);
-    });
-  });
+function runForgeTunnel() {
+  const terminal = vscode.window.createTerminal('Forge Tunnel');
+  terminal.show();
+  terminal.sendText('forge tunnel');
+}
+
+function runForgeDeploy() {
+  const terminal = vscode.window.createTerminal('Forge Deploy');
+  terminal.show();
+  terminal.sendText('forge deploy');
 }
 
 export function deactivate() {}
